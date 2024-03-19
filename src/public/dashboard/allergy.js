7b2d72c40430a4a4f8c55207c6c4826d7983fb43
Atheke <jothharrison@gmail.com>
2024-03-19 17:22:50 +0530

    added requests for allergy medication and history

--- src/public/dashboard/allergy.js
+++ src/public/dashboard/allergy.js
@@ -3,46 +3,46 @@ const form = document.querySelector('.form');
 const allergydiv = document.querySelector('.allergydiv');
 
 
- let book_author = "";
- let book_name = "" ;
- const trigger = document.querySelector('#add-allergy-button'); 
- const close = document.querySelector('.close');
- const submit = document.querySelector('.submit');
- const Library = [];
- let i = 0;// counter for the object index
- //constructor for object book
+let allergy_name = "";
+let allergy_description = "";
+const trigger = document.querySelector('#add-allergy-button');
+const close = document.querySelector('.close');
+const submit = document.querySelector('.submit');
+const Library = [];
+let i = 0;// counter for the object index
+//constructor for object book
 
- patientAllergies.addEventListener('click' , () => {
+patientAllergies.addEventListener('click', () => {
 
-   console.log("we are working");
-    allergydiv.style.display = 'block';
-    medicationdiv.style.display = 'none';
-    historydiv.style.display = 'none';
-    trigger.addEventListener('click' , function()
-        {
-          form.classList.toggle("hidden");//creates class hidden otherwise deletes it
-       });
- 
-       submit.addEventListener('click' , function(event)
-       {
-       event.preventDefault();//prevent the default form submission behaviour
-       //without this the book card gets automatically deleted the first time as the default submission behaviour reloads the page
-       book_author = document.getElementById('allergy').value;
-       book_name = document.getElementById('description').value;
-       const book = new Book(book_author , book_name );
-       Library.push(book);
-      create_book(book);
-       form.classList.toggle("hidden");
-       });
+
+  allergydiv.style.display = 'block';
+  medicationdiv.style.display = 'none';
+  historydiv.style.display = 'none';
+  questions.style.visibility = 'none';
+  
+  trigger.addEventListener('click', function () {
+    form.classList.toggle("hidden");//creates class hidden otherwise deletes it
+  });
+
+  submit.addEventListener('click', function (event) {
+    event.preventDefault();//prevent the default form submission behaviour
+    //without this the book card gets automatically deleted the first time as the default submission behaviour reloads the page
+    const allergies = {
+      allergy: []
+    }
+    allergy_name = document.getElementById('allergy').value;
+    allergy_description = document.getElementById('description').value;
+    allergies.allergy.push(allergy_name);
+    allergies.allergy.push(allergy_description);
+
+    display_allergy(allergy_name , allergy_description);
+    form.classList.toggle("hidden");
+  });
 
 
 });
 
- function Book(author , title )
- {
- this.author = author;
- this.title = title;
- }
+
 
 //  trigger.addEventListener('click' , function()
 //  {
@@ -60,52 +60,49 @@ const allergydiv = document.querySelector('.allergydiv');
 // create_book(book);
 //  form.classList.toggle("hidden");
 //  });
- //to create the book visually on the webapp
- function create_book(book)
- {
- const library_container = document.querySelector('.Library-container');
- const library_items = document.createElement('div');
- const allergy = document.createElement('div');
- const description = document.createElement('div');
- const option_container = document.createElement('div');
- const read_btn = document.createElement('button');
- const delete_btn = document.createElement('button');
- library_items.classList.add('items');
- allergy.classList.add('book-author');
- description.classList.add('book-name');
- option_container.classList.add('options')
- read_btn.classList.add('read');
- delete_btn.classList.add('delete');
- library_items.data = Library.indexOf(book); // data attribute is cdifferent for each book which is essential for deletion
- allergy.textContent = Library[i].author;
- description.textContent = Library[i].title;
- read_btn.textContent = "Read";
- delete_btn.textContent = "Delete";
- option_container.appendChild(read_btn);
-option_container.appendChild(delete_btn);
- library_items.appendChild(allergy);
- library_items.appendChild(description);
- library_items.appendChild(option_container)
- library_container.appendChild(library_items);
- i = i + 1;
- read_btn.onclick = function()
- {
- if(read_btn.style.backgroundColor == "green")
- {
- read_btn.style.backgroundColor = "white";
- }
- else
- read_btn.style.backgroundColor = "green";
- };
- delete_btn.addEventListener('click' , function()
- {
- if(library_items.data == Library.indexOf(book))
- {
- library_container.removeChild(library_items);
- }
- });
- }
- close.onclick = function(){
- form.classList.toggle("hidden");
- console.log("You exited");
- };
+//to create the book visually on the webapp
+function display_allergy(allergy_name , allergy_description) {
+  const library_container = document.querySelector('.Library-container');
+  const library_items = document.createElement('div');
+  const allergy = document.createElement('div');
+  const description = document.createElement('div');
+  
+  const delete_btn = document.createElement('button');
+  library_items.classList.add('items');
+  allergy.classList.add('allergyName');
+  description.classList.add('allergyDescription');
+  
+
+  delete_btn.classList.add('delete');
+  
+  allergy.textContent = allergy_name;
+  description.textContent = allergy_description;
+ 
+  delete_btn.textContent = "Delete";
+
+  
+  library_items.appendChild(allergy);
+  library_items.appendChild(description);
+  library_items.appendChild(delete_btn);
+  library_container.appendChild(library_items);
+  
+
+  delete_btn.addEventListener('click', function () {
+    const parentdiv = delete_btn.parentNode;
+    const data = parentdiv.querySelector('.allergyName').textContent;
+    console.log(data);
+    axios.post('http://localhost:3000/dashboard/allergers' ,data)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    library_container.removeChild(library_items); 
+  });
+}
+close.onclick = function () {
+  form.classList.toggle("hidden");
+  console.log("You exited");
+};
